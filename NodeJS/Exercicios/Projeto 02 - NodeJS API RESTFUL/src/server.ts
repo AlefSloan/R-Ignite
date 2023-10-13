62bb9cf9c1a633b9fe4ccdaf1af0ab3f53f04f4d
alefsloan <alef_sloan@hotmail.com>
2023-10-13 16:40:33 -0300

    install knex sqlite3 and create test route

--- NodeJS/Exercicios/Projeto 02 - NodeJS API RESTFUL/src/server.ts
+++ NodeJS/Exercicios/Projeto 02 - NodeJS API RESTFUL/src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 
+import { knex } from './database'
+
 const app = fastify()
 
-app.get('/test', () => {
-  return 'Hello World'
+app.get('/test', async () => {
+  return await knex('sqlite_schema').select('*')
 })
 
 app

80fc8dda00bcd4de74fb8bb8c1752ba1d47430f3
alefsloan <alef_sloan@hotmail.com>
2023-10-14 20:40:02 -0300

    create tests for getTransaction and summary

--- NodeJS/Exercicios/Projeto 02 - NodeJS API RESTFUL/test/transactions.spec.ts
+++ NodeJS/Exercicios/Projeto 02 - NodeJS API RESTFUL/test/transactions.spec.ts
@@ -51,4 +51,67 @@ describe('Transactions Route', () => {
       }),
     ])
   })
+
+  it('Should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('Should be able to get the summary of all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'New Transaction 2',
+        amount: 3000,
+        type: 'debit',
+      })
+
+    const summaryTransactionsResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryTransactionsResponse.body.summary).toEqual(
+      expect.objectContaining({
+        amount: 2000,
+      }),
+    )
+  })
 })

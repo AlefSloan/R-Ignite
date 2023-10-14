ea356e372ae2481f96f5a0607543c98448bd33f6
alefsloan <alef_sloan@hotmail.com>
2023-10-14 18:55:52 -0300

    create test.db for tests and update .env

--- NodeJS/Exercicios/Projeto 02 - NodeJS API RESTFUL/test/transactions.spec.ts
+++ NodeJS/Exercicios/Projeto 02 - NodeJS API RESTFUL/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { expect, it, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
+import { execSync } from 'child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -9,6 +10,11 @@ describe('Transactions Route', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('Should be able to create a new transaction', async () => {
